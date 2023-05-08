5442c693f3d8d97dc5b2dc1cdd977e166169bebd
EnormouseJohnn <109089482+Alianshark@users.noreply.github.com>
2023-05-08 21:01:34 +0300

    add variable isVacancyToday

--- app.js
+++ app.js
@@ -8,35 +8,35 @@ import { promises as fs } from 'fs';
   var options = {
     day: "numeric",
     month: "long",
-};
-const formatedToday = now.toLocaleDateString("uk-UA",options)
-  //const cookiesString = await fs.readFile('./cookies.json');
-  //const cookies = JSON.parse(cookiesString);
-  //await page.setCookie(...cookies)
-signIn()
-async function  signIn() {
-  await page.goto('https://djinni.co/login?from=frontpage_main');
+  };
+  const formatedToday = now.toLocaleDateString("uk-UA",options)
+  const cookiesString = await fs.readFile('./cookies.json');
+  const cookies = JSON.parse(cookiesString);
+  await page.setCookie(...cookies)
+    //signIn()
+  async function  signIn() {
+    await page.goto('https://djinni.co/login?from=frontpage_main');
 
-  await page.type('#email', process.env.email);
-  await page.type('#password', process.env.password);
+    await page.type('#email', process.env.email);
+    await page.type('#password', process.env.password);
 
-  const signInButtonSelector =  '.btn-primary';
-  const signInButtonElement = await page.waitForSelector(signInButtonSelector);
-  await page.click(signInButtonSelector);
+    const signInButtonSelector =  '.btn-primary';
+    const signInButtonElement = await page.waitForSelector(signInButtonSelector);
+    await page.click(signInButtonSelector);
 
-  const cookies = await page.cookies();
-  console.log(cookies)
-  await fs.writeFile('./cookies.json', JSON.stringify(cookies, null, 2));
-}
+    const cookies = await page.cookies();
+    console.log(cookies)
+    await fs.writeFile('./cookies.json', JSON.stringify(cookies, null, 2));
+  }
 
-//await page.goto('https://djinni.co/jobs/?location=kyiv&region=UKR&primary_keyword=JavaScript');
+  await page.goto('https://djinni.co/jobs/?location=kyiv&region=UKR&primary_keyword=JavaScript&exp_level=no_exp');
 
-await page.setViewport({width: 1080, height: 1024});
+  await page.setViewport({width: 1080, height: 1024});
 
-const vacancyDateSelector = '.text-date';
-const element = await page.waitForSelector(vacancyDateSelector);
+  const vacancyDateSelector = '.text-date';
+  const element = await page.waitForSelector(vacancyDateSelector);
 
-const vacancyDate = await element?.evaluate(el => {
+  const vacancyDate = await element?.evaluate(el => {
     return getFirstNetxNode(el)
     function getFirstNetxNode(el) {
       let iter = document.createNodeIterator(el, NodeFilter.SHOW_TEXT);
@@ -48,9 +48,10 @@ const vacancyDate = await element?.evaluate(el => {
     }
   });
   const vacancyDateTrimmed = vacancyDate.trim()
+  const isVacancyToday = vacancyDateTrimmed === 'сьогодні';
 
-  if (vacancyDateTrimmed === 'сьогодні') {
-    console.log('сьогоднішня ваканція')
+  if (!isVacancyToday) {
+    console.log('Ne сьогоднішня ваканція')
     const vacancyLinkSelector =  '.profile';
     const vacansyLinkElement = await page.waitForSelector(vacancyLinkSelector);
     await page.click(vacancyLinkSelector);
