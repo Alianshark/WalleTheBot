f2f27e0380aacb7ebd81d342bb92441c404cfaed
hellgamer <19thanks96@gmail.com>
2023-05-08 23:03:11 +0300

    apply for the first vacancy

--- app.js
+++ app.js
@@ -70,9 +70,11 @@ import { promises as fs } from 'fs';
     const signInButtonElement = await page.waitForSelector(replyButtonSelector);
     await page.click(replyButtonSelector);
 
-    await page.type('#message', 'Hello I am Vall-E, bot who search vacancies');
+    await page.type('#message', 'Hello I am Wall-E, bot who search vacancies. I am glad to write you. My author is looking for job');
 
-
+    const jobApplyButton =  '#job_apply';
+    const checkWorkButtonElement = await page.waitForSelector(jobApplyButton);
+    await page.click(jobApplyButton);
   }
   console.log('formatedToday', formatedToday)
 
