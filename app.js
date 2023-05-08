4ae9bce25246170166436d66ca84beee17a22a83
EnormouseJohnn <109089482+Alianshark@users.noreply.github.com>
2023-05-08 18:25:02 +0300

    add current day in ua lang without year

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ import puppeteer from 'puppeteer';
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+  const now = new Date();
+  var options = {
+    day: "numeric",
+    month: "long",
+};
+console.log('time',now.toLocaleDateString("uk-UA",options))
 
-  await page.goto('https://djinni.co/jobs/?location=kyiv&region=UKR&primary_keyword=JavaScript&exp_level=no_exp');
 
-  await page.setViewport({width: 1080, height: 1024});
+await page.goto('https://djinni.co/jobs/?location=kyiv&region=UKR&primary_keyword=JavaScript&exp_level=no_exp');
 
-  const vacancyDateSelector = '.text-date';
-  const element = await page.waitForSelector(vacancyDateSelector);
-  
-  const vacancyDate = await element?.evaluate(el => {
+await page.setViewport({width: 1080, height: 1024});
+
+const vacancyDateSelector = '.text-date';
+const element = await page.waitForSelector(vacancyDateSelector);
+
+const vacancyDate = await element?.evaluate(el => {
     return getFirstNetxNode(el)
     function getFirstNetxNode(el) {
       let iter = document.createNodeIterator(el, NodeFilter.SHOW_TEXT);
