27b55f5d292641670b64b3300131d1b6779323e6
hellgamer <19thanks96@gmail.com>
2023-05-08 15:05:01 +0300

    delete useless code from copied example

--- app.js
+++ app.js
@@ -6,25 +6,11 @@ import puppeteer from 'puppeteer';
 
   await page.goto('https://developer.chrome.com/');
 
-  // Set screen size
   await page.setViewport({width: 1080, height: 1024});
 
-  // Type into search box
-  await page.type('.search-box__input', 'automate beyond recorder');
 
-  // Wait and click on first result
-  const searchResultSelector = '.search-box__link';
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
 
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    'text/Customize and automate'
-  );
-  const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
+  console.log('Close Browser');
 
   await browser.close();
 })();
