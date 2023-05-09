53531eda829cf03095c4fb77de115ff90c3950d2
Enormouse <johnnysalvia@gmail.com>
2023-05-10 01:14:02 +0300

    after trying to apply more filters crash bot

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import { goToVacancyWithFilter } from './filter.js'
 import { replyToVacancy, clickOnFirstVacancy } from './vacancy.js'
 import 'dotenv/config'
 
-async function runBot() {
-  const browser = await puppeteer.launch({ headless: true })
+async function runBot(url) {
+  const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
   const today = new Date()
   console.log('\n')
@@ -15,16 +15,9 @@ async function runBot() {
   await page.setViewport({ width: 1080, height: 1024 })
 
   await signInIfNoCookies(page)
-  const newSerchFilter = [
-    'https://djinni.co/jobs/?location=kyiv&region=UKR&primary_keyword=JavaScript&exp_level=no_exp',
-    'https://djinni.co/jobs/?location=kyiv&region=UKR&keywords=react&all-keywords=&any-of-keywords=&exclude-keywords=&exp_level=no_exp',
-  ]
 
-  console.log(newSerchFilter)
-  newSerchFilter.forEach(gotoPage)
-  async function gotoPage(url) {
-    await page.goto(url)
-  }
+  await page.goto(url)
+
   await clickOnFirstVacancy(page)
 
   const allreadyAppliedToVacancy = await checkIfAlreadyAppliedVacancy(page)
@@ -35,7 +28,18 @@ async function runBot() {
   }
 
   await browser.close()
-  setTimeout(runBot, 20_000)
+  setTimeout(runBotForEachUrl, 20_000)
 }
 
-runBot()
+const urls = [
+  'https://djinni.co/jobs/?location=kyiv&region=UKR&primary_keyword=JavaScript&exp_level=no_exp',
+  'https://djinni.co/jobs/?location=kyiv&region=UKR&keywords=react&all-keywords=&any-of-keywords=&exclude-keywords=&exp_level=no_exp',
+]
+async function runBotForEachUrl() {
+  console.log('runBotForEachUrl')
+  for (const url of urls) {
+    console.log('runBotForEachUrl for such url:', url)
+    await runBot(url)
+  }
+}
+runBotForEachUrl()
--- vacancy.js
+++ vacancy.js
@@ -11,13 +11,44 @@ export async function replyToVacancy(page) {
 
     await page.type(
       '#message',
-      'Hello I am Wall-E, bot who search vacancies. I am glad to write you. My author is looking for job'
+      `Dear Hiring Manager,
+
+      I hope this letter finds you well. My name is Ivan Andryevskiy and I am writing about the Javascript Developer role.
+      
+      I am very interested in this opportunity, and I  have finished many Javascript courses. So I am certain I can be useful to you.
+      
+      I have developed 10+ pet project, like:
+      
+      - Javascript arcade game with player flying plane, fire rockets into enemies
+      
+      - HTML5 implementation of the "words in a jar" game
+      
+      - Slot Machine
+      
+      - Bot to search and apply vacancy ( the one who writing you:)) 
+      
+      In my resume, you can find more information on the skills and qualifications I would bring to the table, such as:
+      
+      HTML5, css3, sass
+      flex, grid, keyframes
+      Javascript, React, es6 module
+      JSON, API, telegrafJs, Sql, node.js express
+      Game development, animation, web game performance
+      Git, git bash
+      
+      
+      I'd be happy to provide greater detail about my skills and experience during an interview. 
+      Please call me at your earliest convenience.
+      
+      Sincerely,
+      
+      Ivan Andryevskiy`
     )
 
     const jobApplyButton = '#job_apply'
     const checkWorkButtonElement = await page.waitForSelector(jobApplyButton)
     await page.click(jobApplyButton)
-  } catch {
+  } catch (error) {
     console.log('Не вдалось відгукнутись на ваканцію. Error: ', error)
   }
 }
