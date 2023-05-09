0c471889c06346890afad8c14f4780808c6ccc1d
hellgamer <19thanks96@gmail.com>
2023-05-09 14:12:17 +0300

    create function reply to vacancy

--- app.js
+++ app.js
@@ -66,19 +66,22 @@ import { promises as fs } from 'fs';
     const vacansyLinkElement = await page.waitForSelector(vacancyLinkSelector);
     await page.click(vacancyLinkSelector);
 
-    
-    const replyButtonSelector =  'text/Відгукнутися на вакансію';
-    const signInButtonElement = await page.waitForSelector(replyButtonSelector);
-    await page.click(replyButtonSelector);
-
-    await page.type('#message', 'Hello I am Wall-E, bot who search vacancies. I am glad to write you. My author is looking for job');
-
-    const jobApplyButton =  '#job_apply';
-    const checkWorkButtonElement = await page.waitForSelector(jobApplyButton);
-    await page.click(jobApplyButton);
+    await replyToVacancy(page);
   console.log('formatedToday', formatedToday)
 
   //await browser.close();
 
 })();
+
+async function replyToVacancy(page) {
+  const replyButtonSelector = 'text/Відгукнутися на вакансію';
+  const signInButtonElement = await page.waitForSelector(replyButtonSelector);
+  await page.click(replyButtonSelector);
+
+  await page.type('#message', 'Hello I am Wall-E, bot who search vacancies. I am glad to write you. My author is looking for job');
+
+  const jobApplyButton = '#job_apply';
+  const checkWorkButtonElement = await page.waitForSelector(jobApplyButton);
+  await page.click(jobApplyButton);
+}
 
