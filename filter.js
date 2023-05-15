bff50eed20f5e77766423b8b9793e117f221eade
hellgamer <19thanks96@gmail.com>
2023-05-15 18:55:44 +0300

    add new filter: for me

--- filter.js
+++ filter.js
@@ -1,4 +1,5 @@
 export const filterUrls = [
   'https://djinni.co/jobs/?location=kyiv&region=UKR&primary_keyword=JavaScript&exp_level=no_exp',
   'https://djinni.co/jobs/?location=kyiv&region=UKR&keywords=react&all-keywords=&any-of-keywords=&exclude-keywords=&exp_level=no_exp',
+  'https://djinni.co/jobs/my-profile/',
 ]
